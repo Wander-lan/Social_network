7ee3fdcea68ef14d019fb03832b5fb65655f8c82
Wanderlan Alves <westynger98@gmail.com>
2024-02-06 16:07:13 -0300

    (feat): add database configuration and .env variables

---
+++ api/sql/database.sql
@@ -0,0 +1,14 @@
+/* Use this commands to create the database with mysql */
+CREATE DATABASE IF NOT EXISTS SocialNetwork;
+USE SocialNetwork;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+    id int auto_increment primary key,
+    name varchar(50) not null,
+    nick varchar(50) not null unique,
+    email varchar(50) not null unique,
+    password varchar(20) not null,
+    createdAt timestamp default current_timestamp()
+) ENGINE=INNODB;
