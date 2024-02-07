e2338fef4e104d44b6632c945c30d80a9e647fef
Wanderlan Alves <westynger98@gmail.com>
2024-02-07 17:49:33 -0300

    (feat): add detailed errors and responses to the requests

---
+++ api/src/responses/responses.go
@@ -0,0 +1,25 @@
+package responses
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// Returns a JSON response for the request
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.WriteHeader(statusCode)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Returns a error in JSON format
+func Error(w http.ResponseWriter, statusCode int, err error) {
+	JSON(w, statusCode, struct {
+		Error string `json:"error"`
+	}{
+		Error: err.Error(),
+	})
+}
